2f0435e1cf1b7eaec1fb95a0074aea91c0629c64
neennera <neennera@gmail.com>
2022-09-20 22:49:58 +0700

    table complete

--- Lecture/SegmentTree/tempCodeRunnerFile.cpp
+++ Lecture/SegmentTree/tempCodeRunnerFile.cpp
@@ -1,5 +1,45 @@
-cout << endl << "------\n";
-        for(auto [a,b]:t){
-            cout << a << ',' << b << " ";
+
+        cout << l <<"," << r << "\n";
+        if(l>=ql&&r<=qr){
+            lazy[node]=k;
+            prop(node,l,r);
         }
-        cout << endl;
+
+        int child=node*2, m=(l+r)>>1;
+        update(child,k,l,m,ql,qr);
+        update(child+1,k,m+1,r,ql,qr);
+
+        seg[node]=min(seg[child],seg[child+1]);
+    }
+
+    int get_value(int node,int l,int r,int ql,int qr){
+        if(l>qr||r<ql) return INT_MAX;
+        if(l==r){
+            prop(node,l,r);
+            return seg[node];
+        }
+
+        prop(node,l,r);
+        int child=node*2, m=(l+r)>>1;
+        return min(get_value(child,l,m,ql,qr),get_value(child+1,m+1,r,ql,qr));
+    }
+
+    void pf(){
+        cout << "\npf(); \n";
+        for(int i=1;i<2*C;i++){
+            for(int j=i;j<2*i;j++){
+                cout << seg[i] << " ";
+            }
+            cout << endl;
+        }
+
+        for(int i=1;i<2*C;i++){
+            for(int j=i;j<2*i;j++){
+                cout << lazy[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
